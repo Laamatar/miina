e62f7b2f7873bcc83e90537b7ee7c492f1bf23ce
Siiri <Siiri@10.1.16.245>
2014-08-23 16:46:02 +0300

    mainiin kamaa

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -1,11 +1,18 @@
-
 package miina;
 
+import java.util.Scanner;
+
 public class Miina {
+
     public static void main(String[] args) {
-        
+        Scanner lukija = new Scanner(System.in);
+        System.out.println("Anna vaakakoordinaatti: ");
+        String kirjain = lukija.nextLine();
+        System.out.println("Anna pystykoordinaatti: ");
+        int numero = Integer.parseInt(lukija.nextLine());
+
         koordinaatisto meh = new koordinaatisto();
         meh.tulosta();
 
-    }    
+    }
 }
