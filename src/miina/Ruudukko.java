7fe7c92d89c2dcee418513201990487f1ebd3cee
Siiri <Siiri@10.1.16.245>
2014-08-23 15:06:13 +0300

    osaa tulostaa, mutta tarvii tulostusmetodin D:

--- src/miina/Ruudukko.java
+++ src/miina/Ruudukko.java
@@ -3,46 +3,27 @@ package miina;
 import java.util.ArrayList;
 
 public class Ruudukko {
-    
-private ArrayList ruudut;
 
-    public static void AvaaRuutu() {
+    private koordinaatisto ruudut;
 
-    }
-
-    public static void Tulostettava() {
-
-    }
-
-    public Ruudukko() {
-        this.ruudut = new ArrayList();
-        int a = 1;
-        String kirjaimet = "ABCDEFGHIJ";
-        int luku = 0;
-        int numero = 1;
-
-        while (a <= 10) {
-
-            String kirjain = "" + kirjaimet.charAt(luku);
-            this.ruudut.add(new ruutu(kirjain, numero));
-
-            numero++;
-            a++;
-
+    public void AvaaRuutu(String kirjain, int numero) {
+        ruutu kohta = ruudut.haeRuutu(kirjain, numero);
+        if (kohta.OnkoAuki()) {
+            System.out.println();
+        } else {
+            kohta.Aukaise();
+            System.out.println();
         }
-
-        a = 1;
-
     }
 
-    public void tulosta() {
-
-        int as = 0;
-        int ds = this.ruudut.size();
-        while (ds > 0) {
-            System.out.println(this.ruudut.get(as));
-            as++;
-            ds--;
+    public void Tulostettava() {
+        for (int silmukkamuuttuja = 0; silmukkamuuttuja < 100; silmukkamuuttuja++) {
+            ruutu kohta = ruudut.haeppasRuutu(silmukkamuuttuja);
+            if (kohta.OnkoAuki()) {
+                System.out.println();
+            }else {
+                System.out.println("?");
+            }
         }
     }
 }
