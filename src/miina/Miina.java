16355a665915476fa85ef0112996f6fa64961d52
Siiri <Siiri@10.1.16.245>
2014-08-23 10:31:22 +0300

    miu

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -3,12 +3,8 @@ package miina;
 
 public class Miina {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        miu.
-        // TODO code application logic here
+        
     }
     
 }
