5db2fa4873d96baf81903785c1f393da20a98f5f
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-23 10:08:06 +0300

    testi

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -7,6 +7,7 @@ public class Miina {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        sjrjsgm
         // TODO code application logic here
     }
     
