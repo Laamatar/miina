7880c30ea86e186a3738aecf3d52d551a3ed7368
Siiri <Siiri@10.1.16.245>
2014-08-23 10:27:03 +0300

    poistin kommentit

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -7,7 +7,7 @@ public class Miina {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Mui.
+        miu
         // TODO code application logic here
     }
     
