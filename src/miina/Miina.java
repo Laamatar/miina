569759dcbadcb11b555c5381c0ece2d8610cf466
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-23 16:46:29 +0300

    mainia

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -5,7 +5,10 @@ public class Miina {
     public static void main(String[] args) {
         
         koordinaatisto meh = new koordinaatisto();
+        meh.miina3();
+        meh.laskeArvot();
         meh.tulosta();
+        System.out.println(meh.miinoitetutRuudut());
 
     }    
 }
