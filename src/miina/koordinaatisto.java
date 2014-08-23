7152ae0154202e345c8d0c627bcfc5c28c0b6ea4
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-23 12:42:22 +0300

    koordinaatisto luokka

--- src/miina/koordinaatisto.java
+++ src/miina/koordinaatisto.java
@@ -1,16 +1,44 @@
-
 package miina;
 
 import java.util.ArrayList;
+
 public class koordinaatisto {
-        private ArrayList ruudut;
+
+    private ArrayList ruudut;
+
+    public koordinaatisto() {
+        this.ruudut = new ArrayList();
+        int a = 1;
+        String kirjaimet = "ABCDEFGHIJ";
+        int luku = 0;
+        int numero = 1;
+
+        while (a <= 10) {
+
+            String kirjain = "" + kirjaimet.charAt(luku);
+            this.ruudut.add(new ruutu(kirjain, numero));
+            
+            numero++;
+            a++;
+
+        }
         
-        public koordinaatisto(){
-            this.ruudut = new ArrayList();
-            
-            
-            
+        a = 1;
+        
+        
+        
+
+    }
+
+    public void tulosta() {
+
+        int as = 0;
+        int ds = this.ruudut.size();
+        while (ds > 0) {
+            System.out.println(this.ruudut.get(as));
+            as++;
+            ds--;
         }
     }
-    
-    
+
+}
