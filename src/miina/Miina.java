85bb4e8d297a66b60f05db4be4ff644e2a23d2f6
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-23 10:03:17 +0300

    enismmäinen versio

---
+++ src/miina/Miina.java
@@ -0,0 +1,13 @@
+
+package miina;
+
+public class Miina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
