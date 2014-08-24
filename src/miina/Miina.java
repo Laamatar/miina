ab086c88385db7852ade5f2e2a6e1ad307bb2aef
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-24 13:30:21 +0300

    Merge origin/master
    Conflicts:
    	src/miina/Miina.java

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -1,5 +1,6 @@
 package miina;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Miina {
@@ -15,8 +16,37 @@ public class Miina {
 
         while (true) {
             //ebin tää on infinite loop
-            System.out.println("Anna vaaka- ja pystykoordinaatit (kirjain-numero, ilman välilyöntiä ja isolla): ");
+            System.out.println("Anna vaaka- ja pystykoordinaatit (kirjain-numero, ilman välilyöntiä): ");
             String garr = lukija.nextLine();
+            int syotteenPituus = garr.length();
+            int indeksi = 0;
+            garr = garr.toUpperCase();
+            ArrayList<Integer> syote = new ArrayList<Integer>();
+            while (syotteenPituus > 0) {
+
+                syote.add((int) garr.charAt(indeksi));
+                syotteenPituus--;
+                indeksi++;
+            }
+            if (syote.size() < 2 || syote.size() > 3) {
+                System.out.println("Ei kelpaa.");
+                continue;
+            } else if (syote.get(0) < 'A' || syote.get(0) > 'J') {
+                System.out.println("Ei kelpaa.");
+                continue;
+            } else if (syote.size() == 2 && syote.get(1) < '1') {
+                System.out.println("Ei kelpaa.");
+                continue;
+            } else if (syote.size() == 2 && syote.get(1) > '9') {
+                System.out.println("Ei kelpaa.");
+                continue;
+            } else if (syote.size() == 3 && syote.get(1) != '1') {
+                System.out.println("Ei kelpaa.");
+                continue;
+            } else if (syote.size() == 3 && syote.get(2) > '0') {
+                System.out.println("Ei kelpaa.");
+                continue;
+            }
             String kirjain = "" + garr.charAt(0);
             int numero = Integer.parseInt("" + garr.substring(1));
             System.out.println("");
@@ -28,7 +58,7 @@ public class Miina {
             foo.Tulostettava();
             System.out.println("");
             //System.out.println(meh.miinoitetutRuudut());
-        
+
             if (foo.Havio()) {
                 break;
             }
