78ee937be9b165b5ca3b06f86caf3b261be25512
Siiri <Siiri@10.1.16.245>
2014-08-24 09:07:24 +0300

    tän pitääis myös kai toimia

--- src/miina/Ruudukko.java
+++ src/miina/Ruudukko.java
@@ -4,37 +4,50 @@ import java.util.ArrayList;
 
 public class Ruudukko {
 
-    private koordinaatisto ruudut;
+    public koordinaatisto ruudut;
+    
+    Ruudukko() {
+        
+        ruudut = new koordinaatisto();
+
+        //ruudut.laskeArvot();
+    }
 
     public void AvaaRuutu(String kirjain, int numero) {
         ruutu kohta = ruudut.haeRuutu(kirjain, numero);
         if (kohta.OnkoAuki()) {
-            System.out.println(kohta.annaArvo());
         } else if (kohta.annaArvo() == 0) {
             kohta.Aukaise();
             avaaViereiset(kirjain, numero);
-            System.out.println(kohta.annaArvo());
         } else {
             kohta.Aukaise();
-            System.out.println(kohta.annaArvo());
         }
     }
 
     public void avaaViereiset(String kirjain, int numero) {
         
-        int epanumero = numero - 2;
+        int epanumero = numero-1;
+        String taika = "ABCDEFGHIJKLMNOPQ";
         
         for (int montako = 0; montako < 3; montako++) {
-            int epakirjain = kirjain.charAt(0) - 1;
-            epanumero++;
+            int epakirjain = taika.indexOf(kirjain); //kirjain.charAt(0) - 1;
+            //epanumero++;
 
-            for (int monesko = 0; monesko < 3; monesko++) {
-                
-                ruutu kohta = ruudut.haeRuutu(Integer.toString(epakirjain), epanumero);
-                if (kohta.OnkoAuki() == false && voikoAvata(epakirjain, epanumero)) {
-                    AvaaRuutu(Integer.toString(epakirjain), epanumero);
+            for (int monesko = 0; monesko < 3; monesko++) {   
+                if(epakirjain+montako-1 < 0)
+                    continue;
+                if(epakirjain+montako-1 >= 10 )
+                    continue;
+                if(epanumero+monesko-1 < 0) 
+                    continue;
+                if(epanumero+monesko-1 >= 10 )
+                    continue;
+                //System.out.println("!! " + (epakirjain+montako-1) + " " + (epanumero+monesko-1));
+                ruutu kohta = ruudut.haeRuutu(""+taika.charAt(epakirjain+montako-1), epanumero+monesko);
+                if (kohta.OnkoAuki() == false && voikoAvata(epakirjain+montako-1, epanumero+monesko)) {
+                    AvaaRuutu(""+taika.charAt(epakirjain+montako-1), epanumero+monesko);
                 }
-                epakirjain++;
+                //epakirjain++;
             }
 
         }
@@ -54,10 +67,11 @@ public class Ruudukko {
         for (int silmukkamuuttuja = 0; silmukkamuuttuja < 100; silmukkamuuttuja++) {
             ruutu kohta = ruudut.haeppasRuutu(silmukkamuuttuja);
             if (kohta.OnkoAuki()) {
-                System.out.println(kohta.annaArvo());
+                System.out.print(kohta.annaArvo());
             } else {
-                System.out.println("?");
+                System.out.print("?");
             }
+            if(silmukkamuuttuja%10 == 9) System.out.println();
         }
     }
 }
