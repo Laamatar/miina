691c7938150b87fa1d62f88d44dc1c7a2bd6b534
Siiri <Siiri@10.1.16.245>
2014-08-24 09:06:33 +0300

    mun koodi saattais toimii. paitsi on infinite loop

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -5,16 +5,28 @@ import java.util.Scanner;
 public class Miina {
 
     public static void main(String[] args) {
+        Ruudukko foo = new Ruudukko();
         Scanner lukija = new Scanner(System.in);
-        System.out.println("Anna vaakakoordinaatti: ");
-        String kirjain = lukija.nextLine();
-        System.out.println("Anna pystykoordinaatti: ");
-        int numero = Integer.parseInt(lukija.nextLine());
+        System.out.println("Montako miinaa? ");
+        foo.ruudut.MIINOITA(Integer.parseInt(lukija.nextLine()));
+        
+        while(true){
+        //ebin tää on infinite loop
+        System.out.println("Anna vaaka- ja pystykoordinaatit (kirjain-numero, ilman välilyöntiä ja isolla): ");
+        String garr = lukija.nextLine();
+        String kirjain = "" + garr.charAt(0);
+        int numero = Integer.parseInt(""+garr.charAt(1));
+        
+        
 
-        koordinaatisto meh = new koordinaatisto();
-        meh.laskeArvot();
-        meh.tulosta();
-        System.out.println(meh.miinoitetutRuudut());
+        //koordinaatisto meh = new koordinaatisto();
+        //meh.laskeArvot();
+        //meh.tulosta();
 
+        foo.ruudut.laskeArvot();
+        foo.AvaaRuutu(kirjain, numero);
+        foo.Tulostettava();
+        //System.out.println(meh.miinoitetutRuudut());
+        }
     }
 }
