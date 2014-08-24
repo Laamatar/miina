62330dff338d28e1f4e6d9de8cc75bf09192da66
M.Kääriäinen <M.Kriinen@10.10.16.29>
2014-08-24 10:12:26 +0300

    mainia

--- src/miina/Miina.java
+++ src/miina/Miina.java
@@ -3,19 +3,15 @@ package miina;
 import java.util.Scanner;
 
 public class Miina {
-
+    
     public static void main(String[] args) {
-        Scanner lukija = new Scanner(System.in);
-        System.out.println("Anna vaakakoordinaatti: ");
-        String kirjain = lukija.nextLine();
-        System.out.println("Anna pystykoordinaatti: ");
-        int numero = Integer.parseInt(lukija.nextLine());
-
+        
         koordinaatisto meh = new koordinaatisto();
-        meh.miina3();
+        meh.MIINOITA(10);
         meh.laskeArvot();
+        
         meh.tulosta();
-        System.out.println(meh.miinoitetutRuudut());
-
+        System.out.println(meh.miinoitetutRuudut());    
+        }
     }
-}
+
